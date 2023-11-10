1154294b9baabcc2474c7719db1a6bb480956623
gedeonagmas <103260131+gedeonagmas@users.noreply.github.com>
2023-11-10 11:48:27 +0300

    Update upload.js

--- server/utils/upload.js
+++ server/utils/upload.js
@@ -1,8 +1,6 @@
 const multer = require("multer");
 const diskStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "./uploads");
-  },
+  destination: (req, file, cb) => 
   filename: (req, file, cb) => {
     cb(null, file.fieldname + Date.now() + Math.round(Math.random() * 1e9) + "." + file.mimetype.split("/")[1]);
   },
