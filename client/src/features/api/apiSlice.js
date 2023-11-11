d0fd3ff62097efd99ba74a8fba64080b54770955
gedeonagmas <103260131+gedeonagmas@users.noreply.github.com>
2023-11-11 14:13:16 +0300

    Update apiSlice.js

--- client/src/features/api/apiSlice.js
+++ client/src/features/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000",
+    baseUrl: "https://chat-app-server-eight-snowy.vercel.app",
   }),
   tagTypes: ["get-all-users", "get-history"],
   endpoints: (builder) => ({
